1d426002e72aa411a5ebe116d038641d684f63bf
klezi10 <74952593+klezi10@users.noreply.github.com>
2022-01-06 11:54:48 +0700

    added remaining card text and api

--- script.js
+++ script.js
@@ -2,6 +2,8 @@ let deckId;
 const newDeck = document.getElementById('new-deck');
 const drawBtn = document.getElementById('draw-btn');
 const cardsHolder = document.getElementById('cards');
+const remainingCards = document.getElementById('remaining');
+const gameText = document.getElementById('game-header');
 
 newDeck.addEventListener('click', handleNewDeck);
 
@@ -9,9 +11,8 @@ function handleNewDeck() {
   fetch('https://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=1')
     .then((response) => response.json())
     .then((data) => {
-      console.log(data);
       deckId = data.deck_id;
-      console.log(deckId);
+      remainingCards.textContent = `Remaining: ${data.remaining}`;
     });
 }
 
@@ -26,5 +27,50 @@ drawBtn.addEventListener('click', () => {
       cardsHolder.children[1].innerHTML = `
       <img src=${data.cards[1].image} class="drawn-card" />
       `;
+      const winningText = determineWinningCard(data.cards[0], data.cards[1]);
+      gameText.textContent = winningText;
+      remainingCards.textContent = `Remaining: ${data.remaining}`;
     });
 });
+
+function determineWinningCard(card1, card2) {
+  const valueOptions = [
+    '2',
+    '3',
+    '4',
+    '5',
+    '6',
+    '7',
+    '8',
+    '9',
+    '10',
+    'JACK',
+    'QUEEN',
+    'KING',
+    'ACE',
+  ];
+
+  const card1ValueIndex = valueOptions.indexOf(card1.value);
+  const card2ValueIndex = valueOptions.indexOf(card2.value);
+  console.log('card 1:', card1ValueIndex);
+  console.log('card 2:', card2ValueIndex);
+
+  if (card1ValueIndex > card2ValueIndex) {
+    return 'Computer wins!';
+  } else if (card1ValueIndex < card2ValueIndex) {
+    return 'You win!';
+  } else {
+    return 'War!';
+  }
+}
+
+//========== TESTING =========
+// const card1Obj = {
+//   value: 'KING',
+// };
+
+// const card2Obj = {
+//   value: 'KING',
+// };
+
+// determineWinningCard(card1Obj, card2Obj);
